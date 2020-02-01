55ca652be6370e1fd62066f6708e26a0b2a84b12
Nadiia Vlasenko <ciklum.vlasenko@extaccount.com>
2020-02-01 23:17:47 +0200

    Fix complexity

--- src/noTwoSlash.kt
+++ src/noTwoSlash.kt
@@ -1,16 +1,16 @@
 import java.util.*
 
 fun noTwoSlash(url: String): String {
-    val list = LinkedList(url.toList())
+    val list = url.toList()
+    val newList = mutableListOf(list[0])
     var i = 1
     while (i < list.size) {
-        if (list[i - 1] == '/' && list[i] == '/') {
-            list.removeAt(i)
-        } else i++
+        if (!(list[i - 1] == '/' && list[i] == '/')) newList.add(list[i])
+        i++
     }
-    return list.joinToString(separator = "")
+    return newList.joinToString(separator = "")
 }
 
 fun main() {
-    print(noTwoSlash("/page1///page2////page3/test.html"))
+    print(noTwoSlash("//page1///page2////page3/test.html"))
 }
