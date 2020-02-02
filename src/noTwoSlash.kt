ac73defd24cabe2b33ebecdb96ae13adec976310
Nadiia Vlasenko <ciklum.vlasenko@extaccount.com>
2020-02-02 12:07:42 +0200

    Useless string to list

--- src/noTwoSlash.kt
+++ src/noTwoSlash.kt
@@ -1,16 +1,25 @@
 import java.util.*
 
 fun noTwoSlash(url: String): String {
-    val list = url.toList()
-    val newList = mutableListOf(list[0])
-    var i = 1
-    while (i < list.size) {
-        if (!(list[i - 1] == '/' && list[i] == '/')) newList.add(list[i])
-        i++
+    val newList = mutableListOf<Char>()
+    if (url.isNotEmpty()) {
+        newList.add(url[0])
+        var i = 1
+        while (i < url.length) {
+            if (!(url[i - 1] == '/' && url[i] == '/')) newList.add(url[i])
+            i++
+        }
     }
-    return newList.joinToString(separator = "")
+    return "" + newList.joinToString(separator = "")
 }
 
 fun main() {
-    print(noTwoSlash("//page1///page2////page3/test.html"))
+    println(noTwoSlash("//page1///page2////page3/test.html"))
+    println(noTwoSlash("///////page1///page2////page3/test./as////"))
+    println(noTwoSlash("///"))
+    println(noTwoSlash(""))
+    println(noTwoSlash("a"))
+    println(noTwoSlash("a/b/c"))
+    println(noTwoSlash(""))
+
 }
